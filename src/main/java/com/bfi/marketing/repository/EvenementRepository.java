21d1160c9c346a6e3694d14c0320a0d9d4d3bfb3
AzzaMaalej <maroua.arifa@esprit.tn>
2022-01-04 23:21:27 +0100

    Update EvenementRepository.java

--- src/main/java/com/bfi/marketing/repository/EvenementRepository.java
+++ src/main/java/com/bfi/marketing/repository/EvenementRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface EvenementRepository extends JpaRepository<Evenement,Long> {
-    @Query("select e from Evenement e where e.titre like %:titre% ")
+    @Query("select e from Evenement e where e.titre like %:titre%  or cast( e.date as string) like %:titre%")
     List<Evenement> findByTitre(String titre);
 
     @Query("select e from Evenement e where cast( e.date as string) like %:date% ")
